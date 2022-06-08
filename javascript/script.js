c3e42a693c92fa40963a57c37366c0cd07876a71
Joyin2 <joyinlaskar2000@gmail.com>
2022-06-08 08:44:43 -0700

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,62 @@
+// burger = document.querySelector('.burger')
+// navcontainer = document.querySelector('.navcontainer')
+// navbar = document.querySelector('.navbar')
+// logo = document.querySelector('.logo')
+
+// burger.addEventListener('click', () => {
+//     navbar.classList.toggle('v-class-resp');
+//     logo.classList.toggle('v-class-resp');
+//     navcontainer.classList.toggle('h-nav-resp');
+
+// })
+var responsiveSlider = function () {
+  var slider = document.getElementById("slider");
+  var sliderWidth = slider.offsetWidth;
+  var slideList = document.getElementById("slideWrap");
+  var count = 1;
+  var items = slideList.querySelectorAll("li").length;
+  var prev = document.getElementById("prev");
+  var next = document.getElementById("next");
+
+  window.addEventListener("resize", function () {
+    sliderWidth = slider.offsetWidth;
+  });
+
+  var prevSlide = function () {
+    if (count > 1) {
+      count = count - 2;
+      slideList.style.left = "-" + count * sliderWidth + "px";
+      count++;
+    } else if ((count = 1)) {
+      count = items - 1;
+      slideList.style.left = "-" + count * sliderWidth + "px";
+      count++;
+    }
+  };
+
+  var nextSlide = function () {
+    if (count < items) {
+      slideList.style.left = "-" + count * sliderWidth + "px";
+      count++;
+    } else if ((count = items)) {
+      slideList.style.left = "0px";
+      count = 1;
+    }
+  };
+
+  next.addEventListener("click", function () {
+    nextSlide();
+  });
+
+  prev.addEventListener("click", function () {
+    prevSlide();
+  });
+
+  setInterval(function () {
+    nextSlide();
+  }, 5000);
+};
+
+window.onload = function () {
+  responsiveSlider();
+};
